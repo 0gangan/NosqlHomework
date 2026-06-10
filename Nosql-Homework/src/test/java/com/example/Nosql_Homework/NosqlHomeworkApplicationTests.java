e5c3bbb57036df52a82ba9c099ce81705dfeec3d
XhouganL <163450523+0gangan@users.noreply.github.com>
2026-06-10 15:58:24 +0800

    init:springboot初始化

---
+++ Nosql-Homework/src/test/java/com/example/Nosql_Homework/NosqlHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Nosql_Homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NosqlHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
